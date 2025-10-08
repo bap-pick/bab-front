081126f12a819550e6ebdd58608da041e6b92c8e
Akelsk04 <abdurashitovamadina012@gmail.com>
2025-10-09 00:12:58 +0900

    firebase 연결

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAlmVXljDPwkmi4rik69GLhBIHLskw7P2o",
+  authDomain: "bapick-front.firebaseapp.com",
+  projectId: "bapick-front",
+  storageBucket: "bapick-front.firebasestorage.app",
+  messagingSenderId: "883956844611",
+  appId: "1:883956844611:web:ac74a589aae30be9d0ecf6",
+  measurementId: "G-Z1WEHMPL7F"
+};
